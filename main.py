819b709976b6041283779efe9fe03567ea2555e8
danielbischofff <d.bischoff@student.uni-tuebingen.de>
2026-02-07 11:45:55 +0100

    upload current status

---
+++ main.py
@@ -0,0 +1,96 @@
+from td3_agent import TD3_agent
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import hockey.hockey_env as h_env
+import numpy as np
+import wandb
+
+# ---------------
+# Initialization
+# ---------------
+
+env = h_env.HockeyEnv() 
+obs2 = env.obs_agent_two() # initiate two agents
+
+td3_agent = TD3_agent(env)
+batch_size = td3_agent.config["batch_size"]
+policy_delay = 2
+device = td3_agent.device
+total_it = 0
+
+# import os
+# os.environ["WANDB_MODE"] = "disabled"
+
+# WANDB logging
+run = wandb.init(
+    entity="bischoffd",
+    name="td3_run_002",
+    project="RL_TD3_hockey",
+    config=td3_agent.config,
+) 
+
+# ----------
+# Train run
+# ----------
+
+for eps in range(td3_agent.max_episodes):
+    obs1, info = env.reset()
+    obs2 = env.obs_agent_two()
+    player2 = h_env.BasicOpponent(weak=True)
+    print(f"episode {eps}")
+
+    done = False
+    episode_return = 0
+    
+    while not done:
+        # ---- Buffer Step ----
+        if total_it < td3_agent.config["warm_up"]:
+            # Warmup action
+            a1 = td3_agent.select_random_action()
+        else:
+            # Policy action
+            a1 = td3_agent.select_action_with_policy(obs1)
+
+        if total_it == td3_agent.config["warm_up"]: # warmup finished logging
+            run.log({"event/warmup_finished": 1}, step=total_it)
+
+        a2 = player2.act(obs2)
+
+        # --- Env step ---
+        obs1_new, r, terminated, truncated, info = env.step(np.hstack([a1, a2]))
+        done = terminated or truncated
+        episode_return += r
+        total_it += 1
+
+        # --- Add to Buffer ---
+        td3_agent.buffer.add(obs1, a1, r, obs1_new, done)
+
+        # ---- LEARNING PHASE ----
+        if total_it >= td3_agent.config["warm_up"] and td3_agent.buffer.size >= batch_size:
+
+            state, action, reward, next_state, done_b = td3_agent.buffer.sample(batch_size)
+
+            state_b = torch.FloatTensor(state).to(device)
+            action_b = torch.FloatTensor(action).to(device)
+            reward_b = torch.FloatTensor(reward).to(device)
+            next_state_b = torch.FloatTensor(next_state).to(device)
+            done_b = torch.from_numpy(done_b.astype(np.float32))
+            
+            # ---- CRITIC UPDATE ----
+            critic_loss = td3_agent.critic_update(state_b, action_b, reward_b, next_state_b, done_b)
+            run.log({"critic_loss": critic_loss["critic_loss"], "critic_1_loss": critic_loss["critic_1_loss"], "critic_2_loss": critic_loss["critic_2_loss"]}, step=total_it)
+
+            # ---- ACTOR UPDATE ----
+            if eps % policy_delay == 0:
+                agent_loss = td3_agent.actor_target_update(state_b)
+                run.log({"agent_loss":agent_loss}, step=total_it)
+
+            # --- CHECKPOINTING per step ---
+            if eps > 0 and eps % td3_agent.config["checkpoint_interval"] == 0:
+                td3_agent.save_checkpoint(step=str(total_it))
+
+    run.log({"episode_return": episode_return}, step=total_it)
+
+
+td3_agent.save_checkpoint(step="last")
---
+++ td3_agent.py
@@ -0,0 +1,244 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torch.nn.functional as F
+
+class TD3_agent():
+    def __init__(self, env):
+        self.env = env
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.num_actions = env.num_actions
+        self.observation_space = env.observation_space
+        self.observation_space_dim = env.observation_space.shape[0]
+        self.config = {
+            "batch_size" : 100,
+            "gamma" : 0.99,
+            "tau" : 0.005,
+            "policy_delay" : 2,
+            "warm_up" : 10.000,
+            "max_episodes" : 100,
+            "exploration_noise_s" : 0.1,
+            "target_smoothing_noise_s" : 0.2,
+            "target_noise_clipping": 0.5,
+            "lr_critic" : 1e-3,
+            "lr_actor" : 1e-3,
+            "checkpoint_path": "./checkpoints",
+            "checkpoint_interval": 100
+        }
+        self.max_episodes = self.config["max_episodes"]
+        self.action_bounds = (env.action_space.low[0], env.action_space.high[0])
+        self.initiate_models()
+        self.buffer = ReplayBuffer(state_dim=self.observation_space_dim, action_dim=self.num_actions)
+
+    def initiate_models(self):
+        self.actor = Actor(self.num_actions, self.observation_space_dim, max_action=1)
+        self.critic_1 = Critic(self.num_actions, self.observation_space_dim)
+        self.critic_2 = Critic(self.num_actions, self.observation_space_dim)
+
+        self.actor_target = Actor(self.num_actions, self.observation_space_dim, max_action=1)
+        self.critic_1_target = Critic(self.num_actions, self.observation_space_dim)
+        self.critic_2_target = Critic(self.num_actions, self.observation_space_dim)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.critic_1_target.load_state_dict(self.critic_1.state_dict())
+        self.critic_2_target.load_state_dict(self.critic_2.state_dict())
+
+        critic_params = list(self.critic_1.parameters()) + list(self.critic_2.parameters())
+        self.critic_optimizer = torch.optim.Adam(critic_params, lr=self.config["lr_critic"])
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=self.config["lr_actor"])
+
+    def select_random_action(self):
+        return np.random.uniform(self.action_bounds[0],self.action_bounds[1], self.num_actions).tolist()
+
+    def select_action_with_policy(self, state,  add_noise=True):
+
+        # check if state is numpy and transform to tensor
+        if not isinstance(state, np.ndarray):
+            state = np.array(state, dtype=np.float32)
+        state_t = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+
+        with torch.no_grad():
+            action_t = self.actor(state_t)
+        action = action_t.cpu().numpy()[0]
+
+        if add_noise:
+            noise = np.random.normal(0, self.config["exploration_noise_s"], size=self.num_actions)
+            action = action + noise
+
+        # clip to env bounds
+        action = np.clip(action, self.action_bounds[0], self.action_bounds[1]) # FIXME maybe we need this
+
+        return action.astype(np.float32)
+    
+    def critic_update(self, state, action, reward, next_state, done):
+        
+        # ---- Compute targets (no gradients) ----
+        with torch.no_grad():
+            # generates noise
+            noise = torch.randn_like(action) * self.config["target_smoothing_noise_s"]
+            noise = noise.clamp(
+                -self.config["target_noise_clipping"],
+                self.config["target_noise_clipping"]
+            )
+
+            next_action = self.actor_target(next_state) + noise
+            next_action = next_action.clamp(self.action_bounds[0], self.action_bounds[1])
+
+            target_Q1 = self.critic_1_target(next_state, next_action)
+            target_Q2 = self.critic_2_target(next_state, next_action)
+
+            target_Q = reward + self.config["gamma"]* (1 - done) * torch.min(target_Q1, target_Q2)
+
+        # ---- Critic prediction values ----
+        current_Q1 = self.critic_1(state, action)
+        current_Q2 = self.critic_2(state, action)
+
+        # ---- Critic loss ----
+        critic_1_loss = F.mse_loss(current_Q1, target_Q)
+        critic_2_loss = F.mse_loss(current_Q2, target_Q)
+        critic_loss = critic_1_loss + critic_2_loss 
+
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        return {
+            "critic_loss": critic_loss.item(),
+            "critic_1_loss": critic_1_loss.item(),
+            "critic_2_loss": critic_2_loss.item(),
+        }
+
+
+    def actor_target_update(self, state_batch):
+
+        # Actor loss: maximize Q_1(s, π(s))  -> minimize -Q_1(...)
+        actor_loss = - self.critic_1(state_batch, self.actor(state_batch)).mean()
+
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # ----- Soft update of all target networks -----
+        with torch.no_grad():
+            # actor target
+            tau = self.config["tau"]
+            for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                target_param.data.copy_(tau * param.data + (1.0 - tau) * target_param.data)
+
+            # critic_1 target
+            for param, target_param in zip(self.critic_1.parameters(), self.critic_1_target.parameters()):
+                target_param.data.copy_(tau * param.data + (1.0 - tau) * target_param.data)
+
+            # critic_2 target (if you have a second critic, as in TD3)
+            for param, target_param in zip(self.critic_2.parameters(), self.critic_2_target.parameters()):
+                target_param.data.copy_(tau * param.data + (1.0 - tau) * target_param.data)
+
+        return actor_loss
+
+    def save_checkpoint(self, step, small=False):
+        path = self.config["checkpoint_path"]
+        if small:
+            checkpoint = {
+                "actor": self.actor.state_dict(),
+                "config": self.config,
+            }
+        else:
+            checkpoint = {
+                "actor": self.actor.state_dict(),
+                "critic_1": self.critic_1.state_dict(),
+                "critic_2": self.critic_2.state_dict(),
+                "actor_target": self.actor_target.state_dict(),
+                "critic_1_target": self.critic_1_target.state_dict(),
+                "critic_2_target": self.critic_2_target.state_dict(),
+                "actor_optimizer": self.actor_optimizer.state_dict(),
+                "critic_optimizer": self.critic_optimizer.state_dict(),
+                "replay_buffer": self.buffer,
+                "timestep": step,
+                "config": self.config,
+            }
+        torch.save(checkpoint, f"{path}/td3_checkpoint_{step}.pt")
+    
+    def resume_from_check(self):
+        path = self.config["checkpoint_path"]
+        ckpt = torch.load(f"{path}/td3_checkpoint.pt", map_location=self.device)
+
+        self.actor.load_state_dict(ckpt["actor"])
+        self.critic_1.load_state_dict(ckpt["critic_1"])
+        self.critic_2.load_state_dict(ckpt["critic_2"])
+        self.actor_target.load_state_dict(ckpt["actor_target"])
+        self.critic_1_target.load_state_dict(ckpt["critic_1_target"])
+        self.critic_2_target.load_state_dict(ckpt["critic_2_target"])
+
+        self.actor_optimizer.load_state_dict(ckpt["actor_optimizer"])
+        self.critic_optimizer.load_state_dict(ckpt["critic_optimizer"])
+
+        self.replay_buffer = ckpt["replay_buffer"]
+        t = ckpt["timestep"]
+        self.config = ckpt["config"]
+
+        return t
+
+    # def load_trained_model(self):
+    #     ckpt = torch.load("td3_actor.pt", map_location=self.device)
+
+    #     self.actor.load_state_dict(ckpt["actor"])
+    #     self.actor.to(self.device)
+    #     self.actor.eval()
+
+class Actor(nn.Module):
+    def __init__(self, num_actions, observation_space, max_action=1):
+
+        super().__init__()
+        self.l1 = nn.Linear(observation_space, 400)
+        self.l2 = nn.Linear(400, 300)
+        self.l3 = nn.Linear(300, num_actions)
+        self.max_action = max_action
+
+    def forward(self, x):
+            x = torch.relu(self.l1(x))
+            x = torch.relu(self.l2(x))
+            return self.max_action * torch.tanh(self.l3(x))
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        self.l1 = nn.Linear(state_dim + action_dim, 400)
+        self.l2 = nn.Linear(400, 300)
+        self.l3 = nn.Linear(300, 1)    
+        
+    def forward(self, x, u):
+        x = torch.relu(self.l1(torch.cat([x, u], 1)))
+        x = torch.relu(self.l2(x))
+        return self.l3(x)
+    
+class ReplayBuffer:
+    def __init__(self, state_dim, action_dim, max_size=int(1e6)):
+        self.max_size = max_size
+        self.ptr = 0
+        self.size = 0
+
+        self.states = np.zeros((max_size, state_dim), dtype=np.float32)
+        self.actions = np.zeros((max_size, action_dim), dtype=np.float32)
+        self.next_states = np.zeros((max_size, state_dim), dtype=np.float32)
+        self.rewards = np.zeros((max_size, 1), dtype=np.float32)
+        self.dones = np.zeros((max_size, 1), dtype=bool)
+
+    def add(self, state, action, reward, next_state, done):
+        self.states[self.ptr] = state
+        self.actions[self.ptr] = action
+        self.next_states[self.ptr] = next_state
+        self.rewards[self.ptr] = reward
+        self.dones[self.ptr] = float(done)
+
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample(self, batch_size):
+        idx = np.random.randint(0, self.size, size=batch_size)
+        return (
+            self.states[idx],
+            self.actions[idx],
+            self.rewards[idx],
+            self.next_states[idx],
+            self.dones[idx],
+        )
