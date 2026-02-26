04ea5d6f52965f1226fd109fcb19968e880683c8
Daniel Bischoff <76752272+danielbischofff@users.noreply.github.com>
2026-02-26 22:26:10 +0100

    Delete scripts directory

--- scripts/run_tournament.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export COMPRL_SERVER_URL=comprl.cs.uni-tuebingen.de
-export COMPRL_SERVER_PORT=65335
-export COMPRL_ACCESS_TOKEN=f68284a1-f8b1-4a1d-a716-f342e9b97a06
-
-python3 ./run_client.py --args --agent=td3
